ec2dad21fd88af05d5713ecd89de17ece399ee69
Joe Chaiet <jchaiet@hotmail.com>
2021-01-02 12:00:50 -0500

    Added About image

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,12 +1,18 @@
 import React, { forwardRef } from 'react';
+import noma_1 from '../assets/img/projects/noma-1.png';
+import noma_2 from '../assets/img/projects/noma-2.png';
+import noma_3 from '../assets/img/projects/noma-3.png';
+import noma_4 from '../assets/img/projects/noma-4.png';
+import chatter_1 from '../assets/img/projects/chatter-1.png';
+import joechaiet_1 from '../assets/img/projects/joechaiet-1.png';
 
 import './Projects.scss';
 
 const Projects = forwardRef((props, ref) => {
   const projects = [
-    {name: 'Noma', desc: 'Noma is a rental management application.', images: [1, 2, 3, 4], link: null},
-    {name: 'Chatter', desc: 'Chatter is a React chat application that uses Socket.io.', images: [1], link: null},
-    {name: 'This website!', desc: 'Whoa! Appception! This page is a React app. Simpe but effective. It includes some basic PHP for the contact form.', images: [1], link: null}
+    {name: 'Noma', desc: 'Noma is a rental management application.', images: [noma_1, noma_2, noma_3, noma_4], link: null},
+    {name: 'Chatter', desc: 'Chatter is a React chat application that uses Socket.io.', images: [chatter_1], link: null},
+    {name: 'This website!', desc: 'Whoa! Appception! This page is a React app. Simpe but effective. It includes some basic PHP for the contact form.', images: [joechaiet_1], link: null}
   ];
 
   return (
@@ -23,7 +29,9 @@ const Projects = forwardRef((props, ref) => {
 
                 <div className="list__image-container">
                   { project.images.map((image, i) => (
-                    <div key={i} className="item__image">image</div>
+                    <>
+                      <img key={i} src={image} Alt="project"/>
+                    </>
                   ))}
                 </div>
               </div>
